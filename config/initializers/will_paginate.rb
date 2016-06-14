2cf7f73fd4eda1dfa9fab468810d55381b4b89ce
jebw <jeb@jdwilkins.co.uk>
2016-06-14 15:55:49 +0100

    Fixed links for Prev/Next

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -31,8 +31,8 @@ module WillPaginate
       end
 
       def previous_or_next_page(page, text, classname)
-        tag :li, link(text, page || '#'),
-	    :class => [(classname[0..3] if  @options[:page_links]), (classname if @options[:page_links]), ('disabled' unless page)].join(' ')
+        tag :li, link(text, page || '#', :class => 'page-link'),
+	    :class => [(classname[0..3] if  @options[:page_links]), (classname if @options[:page_links]), ('disabled' unless page), 'page-item'].join(' ')
       end
     end
   end
